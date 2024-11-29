1589645e6271589369b2baf68893038daed043f5
Awe Oluwatimileyin Matthew <105941839+CodeATM@users.noreply.github.com>
2024-11-29 11:54:40 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,43 @@
-<script>
-export default {
-  data() {
-    return {
-      name: "John Doe",
-      status: "yoo",
-      tasks: ['task 1', 'task 2', 'task 3'],
-      link: "https://google.com"
-    }
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === "active") {
-        this.status = "pending";
-      } else if (this.status === "pending") {
-        this.status = "inactive";
-      } else {
-        this.status = "active";
-      }
-    }
+<script setup>
+
+import { onMounted, ref } from 'vue';
+
+const name = ref("John Doe");
+const status = ref("yoo");
+const tasks = ref(['task 1', 'task 2', 'task 3']);
+const link = ref("https://google.com");
+const newTask = ref("");
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
   }
-}
+};
+
+const handleSubmit = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    tasks.value = data.map(task => task.title);
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+  }
+});
+
 </script>
 
 <template>
@@ -27,10 +45,16 @@ export default {
   <p v-if="status === 'active'"> User is active</p>
   <p v-else-if="status === 'pending'"> User is pending </p>
   <p v-else> User is inactive </p>
-
+  <form @submit.prevent="handleSubmit">
+    <input type="text" label='newTask' id='newTask' v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
   <h3>Task:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
   <a :href="link">click to go away</a>
   <button @click="toggleStatus">click to toggle status</button>
